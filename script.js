099edf62b266e67ae19322d4f3b5d0c04500a4f8
GEKO-3 <jazex.000@gmail.com>
2023-12-13 08:52:09 +0500

    scammer tickets and css update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 fetch('text.json')
     .then(response => response.json())
     .then(data => {
-        // Iterate over each key in the JSON
+        
         Object.keys(data).forEach(key => {
-            // Get the ID and text content dynamically
+            
             const elementId = key;
             const dynamicText = data[key];
 
-            // Get the element by ID
+            
             const targetElement = document.getElementById(elementId);
 
-            // Set the text content dynamically
+
             if (targetElement) {
                 targetElement.textContent = dynamicText;
             } else {
